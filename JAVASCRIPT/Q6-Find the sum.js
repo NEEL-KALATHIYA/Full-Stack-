d77df729d92342772e09faae38c09ced136fb9bd
NEEL-KALATHIYA <kalathiyaneel4@gmail.com>
2024-04-18 10:42:51 +0530

    new

--- JAVASCRIPT/Q6-Find the sum.js
+++ JAVASCRIPT/Q6-Find the sum.js
@@ -1,12 +1,8 @@
-var sum = function (a = 0, b = 0, c = 0, d = 0) {
-    let result = a + b + d - c;
-  
-    console.log(result);
-  };
-  
-  const a = Number(process.argv[2]),
-    b = Number(process.argv[3]),
-    c = Number(process.argv[4]),
-    d = Number(process.argv[5]);
-  
-  sum(a, b, c, d);
+let a=1
+let b=2
+let c=3
+let d=4
+
+sum=a+b+d-c
+
+console.log(sum);
