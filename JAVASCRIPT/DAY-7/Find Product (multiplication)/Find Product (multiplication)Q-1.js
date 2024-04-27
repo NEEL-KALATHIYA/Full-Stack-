cb4276041cb02a9ecd1dff3e08f6f62fa1b1117c
NEEL-KALATHIYA <kalathiyaneel4@gmail.com>
2024-04-27 11:14:28 +0530

    new

--- JAVASCRIPT/DAY-7/Find Product (multiplication)/Find Product (multiplication)Q-1.js
+++ JAVASCRIPT/DAY-7/Find Product (multiplication)/Find Product (multiplication)Q-1.js
@@ -1,1 +1,20 @@
-console.log();
+const putNumber = (id) => {
+    return parseInt(document.getElementById(id).value);
+  };
+  
+  const printResult = (result) => {
+    document.getElementById("result").innerHTML = result;
+  };
+  
+  const getResult = () => {
+    const n1 = putNumber("n1"),
+      n2 = putNumber("n2"),
+      n3 = putNumber("n3"),
+      n4 = putNumber("n4"),
+      n5 = putNumber("n5");
+  
+    const result = `Final Product is :  ${n1 * n2 * n3 * n4 * n5}`;
+  
+    printResult(result);
+  };
+  
